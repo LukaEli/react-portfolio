dd8326699db89666e7c0de8872c5102b07af5955
CloneTrooper66 <luka.eli88@gmail.com>
2024-07-18 17:46:44 +0100

    adds navigation section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./nav/Nav.js";
+import About from "./about/About";
+import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
 
 const App = () => {
-  return <div className="App">Test for commit</div>;
+  return (
+    <>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={About}></Route>
+          <Route path="/skills" element={Skills}></Route>
+          <Route path="/projects" element={Projects}></Route>
+          <Route path="/contact" element={Contact}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;

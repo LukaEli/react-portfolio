d0e5c0101d5dc113df3a0d0d347faa4db1de6dee
CloneTrooper66 <luka.eli88@gmail.com>
2024-07-18 21:15:49 +0100

    adds avatar section

--- src/about/AboutMenu.jsx
+++ src/about/AboutMenu.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class About extends Component {
+export default class AboutMenu extends Component {
   render() {
     return <div></div>;
   }

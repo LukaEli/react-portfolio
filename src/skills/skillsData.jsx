c05fc8eae4c9a5444e0b44eab7da79fcd1aeefc1
CloneTrooper66 <luka.eli88@gmail.com>
2024-07-19 12:59:31 +0100

    displays skills with styling

---
+++ src/skills/skillsData.jsx
@@ -0,0 +1,56 @@
+const skills = {
+  1: [
+    {
+      title: "HTML",
+      level: 5,
+    },
+    {
+      title: "CSS",
+      level: 3,
+    },
+    {
+      title: "JavaScript",
+      level: 5,
+    },
+    {
+      title: "REACT JS",
+      level: 4,
+    },
+    {
+      title: " Bootstrap",
+      level: 2,
+    },
+    {
+      title: "Material UI",
+      level: 3,
+    },
+  ],
+  2: [
+    {
+      title: "Node JS",
+      level: 5,
+    },
+    {
+      title: "SQL",
+      level: 3,
+    },
+    {
+      title: "PSQL",
+      level: 3,
+    },
+    {
+      title: "Express JS",
+      level: 4,
+    },
+    {
+      title: "GIT",
+      level: 3,
+    },
+    {
+      title: "Seeding",
+      level: 2,
+    },
+  ],
+};
+
+export default skills;

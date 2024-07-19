5fb75c8689f813ea4e68b5c57d857bf0ce5c5efc
CloneTrooper66 <luka.eli88@gmail.com>
2024-07-19 10:53:14 +0100

    adds data for education and career section

---
+++ src/about/AboutSubheadingData.jsx
@@ -0,0 +1,74 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2020 Level 3",
+      content: (
+        <>
+          <p>Access to Higher Education Diploma (Computer Science and Maths)</p>
+        </>
+      ),
+    },
+    {
+      title: "2014 Bachelors Degree",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2024 Trainee Full Stack Developer",
+      content: (
+        <>
+          <p>
+            Completed an intensive 13-week coding boot camp specializing in
+            full-stack JavaScript development at Northcoders, a prestigious UK
+            tech education provider known for its industry-focused boot camps
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2022 Team Leader",
+      content: (
+        <>
+          <p>
+            Manage a team of nine staff members to ensure timely production of
+            food items for distribution to three Wasabi stores
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2017 IT Consultant",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
